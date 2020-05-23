84a4342a70ca7840f2cb5d38381a27f1c9a1e5b4
lramosbia@outlook.com <lramosbia@outlook.com>
2020-05-23 17:21:01 -0300

    alterações ordem

--- Ordem/Ordem.java
+++ Ordem/Ordem.java
@@ -1,8 +1,14 @@
 package Ordem;
+
 import javax.swing.JOptionPane;
+import java.util.List;
+import java.util.ArrayList;
+
+
 
 public class Ordem {
     int x, y, z;
+    // int[] ordemCrescente;
 
     public Ordem(int x, int y, int z)
     {
@@ -10,8 +16,9 @@ public class Ordem {
         this.y = Integer.parseInt(JOptionPane.showInputDialog(null,"Insira um valor inteiro - Y"));;
         this.z = Integer.parseInt(JOptionPane.showInputDialog(null,"Insira um valor inteiro - Z"));;
     }
+    
 
-    public void mostrarMaior(int x, int y, int z)
+    public void mostrarMaior()
     {
         if(x > y && x > z)
         {
@@ -27,7 +34,7 @@ public class Ordem {
         }
     }
 
-    public void mostrarMenor(int x, int y, int z)
+    public void mostrarMenor()
     {
         if(x < y && x < z)
         {
@@ -43,13 +50,54 @@ public class Ordem {
         }
     }
 
-    public void mostrarCrescente(int x, int y, int z)
+    public void mostrarCrescente()
     {
+        System.out.println("Array crescente");
+        List<Integer> ordemCrescente = new ArrayList<Integer>();
+        Object[] arrayCrescente;
 
+        if (x < y && x < z) ordemCrescente.add(x);
+        else if (y < x && y < z) ordemCrescente.add(y);
+        else ordemCrescente.add(z);
+
+        if (x > y && x > z) ordemCrescente.add(1, x);
+        else if (y > x && y > z) ordemCrescente.add(1, y);
+        else ordemCrescente.add(1, y);
+
+        if (!ordemCrescente.contains(x)) ordemCrescente.add(x);
+        else if (!ordemCrescente.contains(y)) ordemCrescente.add(y);
+        else ordemCrescente.add(z);
+
+        arrayCrescente = ordemCrescente.toArray();
+
+        for (int i = 0; i < arrayCrescente.length; i++) {
+        System.out.println(arrayCrescente[i]);
+        }
     }
 
-    public void mostrarDecrescente(int x, int y, int z)
+    public void mostrarDecrescente()
     {
-
+        System.out.println("Array decrescente");
+       
+        List<Integer> ordemDecrescente = new ArrayList<Integer>();
+        Object[] arrayDecrescente;
+        
+        if (x > y && x > z) ordemDecrescente.add(x);
+        else if (y > x && y > z) ordemDecrescente.add(y);
+        else ordemDecrescente.add(y);
+        
+        if (x < y && x < z) ordemDecrescente.add(x);
+        else if (y < x && y < z) ordemDecrescente.add(0, y);
+        else ordemDecrescente.add(0, z);
+        
+        if (!ordemDecrescente.contains(x)) ordemDecrescente.add(0, x);
+        else if (!ordemDecrescente.contains(y)) ordemDecrescente.add(0, y);
+        else ordemDecrescente.add(0, z);
+        
+        arrayDecrescente = ordemDecrescente.toArray();
+        
+        for (int i = 0; i < arrayDecrescente.length; i++) {
+        System.out.println(arrayDecrescente[i]);
+        }        
     }
 }
--- Ordem/TesteOrdem.java
+++ Ordem/TesteOrdem.java
@@ -1,5 +1,16 @@
 package Ordem;
 
 public class TesteOrdem {
-    
+    public static void main(String[] args){
+
+        Ordem ordem = new Ordem(0,0,0);
+
+        ordem.mostrarMaior();
+
+        ordem.mostrarMenor();
+
+        ordem.mostrarCrescente();
+
+        ordem.mostrarDecrescente();
+    }
 }
